4b2d28261f52ce5853ef707f1e5ba326526dad34
Thomas Nilefalk <thomas@junovagen.se>
2018-06-21 20:02:32 +0000

    Add and instantiate an editor template

--- noteit/client/main.js
+++ noteit/client/main.js
@@ -1,22 +1,16 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import SimpleMDE from 'simplemde';
+import {} from 'simplemde/src/css/simplemde.css';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.editor.onRendered(function helloOnCreated() {
+  new SimpleMDE({element: document.getElementById("textarea")});
+}); 
+
+Template.editor.helpers({
+  notes: [
+    ],
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
