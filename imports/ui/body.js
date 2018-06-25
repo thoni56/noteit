8e34aab0167c89a6d3ba4aad4ca8146e44b035c8
Thomas Nilefalk <thomas@junovagen.se>
2018-06-26 00:17:12 +0200

    Use tags collection

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,7 +1,6 @@
 import { Template } from 'meteor/templating';
 import { Notes } from '../api/notes.js';
 import { Tags } from '../api/tags.js';
-import { editor } from './editor.js';
 
 import './note.js';
 import './tag.js';
@@ -13,11 +12,6 @@ Template.body.helpers({
         return Notes.find({}, { sort: { createdAt: -1}});
     },
     tags() {
-        let t = Tags.find({}, { sort: { name: 1}}); 
-        return [
-            { _id: 1, name: 'Tag1' },
-            { _id: 2, name: 'Tag2' },
-            { _id: 3, name: 'Tag3' },
-          ];
+        return Tags.find({}, { sort: { name : 1}});
     },
 });
