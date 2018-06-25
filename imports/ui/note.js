ccba373169d8a4c2172c4d1ecb6618f5a4c39b8e
Thomas Nilefalk <thomas@junovagen.se>
2018-06-25 23:40:05 +0200

    Remove unused stuff

--- imports/ui/note.js
+++ imports/ui/note.js
@@ -1,14 +1,9 @@
 import { Template } from 'meteor/templating';
-import { Notes } from '../api/notes.js';
-import { editor, load } from '../ui/editor';
+import { load } from '../ui/editor';
 
 import './note.html';
 
 Template.note.events({
-    'click .delete'() {
-        Notes.remove(this._id);
-    },
-
     'click .list-group-item'() {
         load(this._id);
     }
