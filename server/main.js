f62b60bd81c92ed09132790281983014ef4ccb58
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2018-06-26 16:23:49 +0200

    Remove the notetags Collection

--- server/main.js
+++ server/main.js
@@ -1,7 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/api/notes.js';
 import '../imports/api/tags.js';
-import '../imports/api/notetags.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
