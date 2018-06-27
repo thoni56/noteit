edc0d6c6a7b9870709b00a89ba1f178e013d1432
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2018-06-28 00:14:10 +0200

    Add tootip and tag count to notes in list

--- imports/ui/note.js
+++ imports/ui/note.js
@@ -3,6 +3,15 @@ import { load } from '../ui/editor';
 
 import './note.html';
 
+Template.note.helpers({
+    tagCount() {
+        if (this.tags) {
+            return this.tags.length;
+        } else {
+            return 0;
+        }
+    }
+})
 Template.note.events({
     'click .list-group-item'() {
         load(this._id);
