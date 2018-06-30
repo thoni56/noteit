93b7288cd1cbc945da6055709e2bea9878f6b2c2
Thomas Nilefalk <thomas@junovagen.se>
2018-06-30 23:40:17 +0200

    Add some handling of active tag

--- imports/ui/tag.js
+++ imports/ui/tag.js
@@ -3,9 +3,24 @@ import { Tags } from '../api/tags.js';
 
 import './tag.html';
 
+var activeTag = new ReactiveVar(undefined);
+
+Template.tag.helpers({
+    active() {
+        if (this._id == activeTag.get()) {
+            return "active";
+        } else {
+            return "";
+        }
+    },
+});
+
 Template.tag.events({
-    'click .tag-list-item'() {
-        console.log("klick tag ", this._id);
+    'click .reset-tag-filter'() {
+        activeTag.set(undefined);
+    },
+    'click .tag'() {
+        activeTag.set(this._id);
     },
     'click .edit-tag'() {
         console.log("edit tag ", this._id);
