af607591bdcc1e5735e86d2fa2e97fd9f3d7e171
Thomas Nilefalk <thomas@nilefalk.se>
2018-08-27 19:04:26 +0200

    Re-add import click handling
    This time with programmatically clicking the input element

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,17 +1,34 @@
 import { Template } from 'meteor/templating';
 import { serializeAllNotes, convertSerializedNotesToCSV } from './exporter/exporter';
+import { csvImporter } from './importer/importer';
 import { saveAs } from 'file-saver/FileSaver';
 
 import './body.html';
 
 Template.body.events({
     'click #exportCSVlink': function () {
-        exportCSV({ filename: "export.csv"});
+        exportCSV({ filename: "export.csv" });
+    },
+    'click #importCSVlink': function () {
+        const element = document.getElementById("importFileName");
+        element.click();
+    },
+    'change #importFileName'(event) {
+        importCSV(event.target.files[0]);
     }
 });
 
 function exportCSV() {
     const csv = convertSerializedNotesToCSV(serializeAllNotes());
-    const blob = new Blob([csv], {type: "text/csv;charset=utf-8"});
-    saveAs(blob, "export.csv");
+    const blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
+    saveAs(blob, "noteit-export.csv");
+}
+
+function importCSV(file) {
+    console.log(file);
+    const reader = new FileReader(file);
+    reader.onload = function () {
+        csvImporter(reader.result);
+    };
+    reader.readAsText(file);
 }
